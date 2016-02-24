86561421ce7232cfc19a265be0018549ed34a86b
戴鑫通 <xintong.dai@wenba100.com>
2016-02-24 13:49:17 +0800

    update
    Signed-off-by: 戴鑫通 <xintong.dai@wenba100.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223074549) do
+ActiveRecord::Schema.define(version: 20160224054810) do
 
   create_table "locks", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160223074549) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.integer  "http_status"
+    t.string   "title"
   end
 
   add_index "pages", ["content_hash"], name: "index_pages_on_content_hash"
